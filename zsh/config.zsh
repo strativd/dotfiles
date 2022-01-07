9773e76f1099b7c21328abfd08b491110a89e726
Strat Barrett <strat.barrett@shopify.com>
2022-01-07 09:34:43 -1000

    History: ignore dups or trailing whitespace 🤷‍♂️

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -26,6 +26,8 @@ setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
+# Ignore history duplicates and commands starting with a whitespace
+export HISTCONTROL=ignoreboth:erasedups
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
