0a5a1741f7f5928d70dca5956452a82180270853
Strat Barrett <strat.barrett@figment.io>
2022-08-05 11:33:01 -0700

    jest: test coverage, directory

---
+++ js/jest.zsh
@@ -0,0 +1,8 @@
+# Jest testing library
+#
+# add a file path after all commands to specify a specific test
+#
+alias jest="yarn jest "
+alias jestc="yarn jest --coverage "
+alias jestcd="yarn jest --coverage --coverageDirectory='coverage' "
+alias jestco="open coverage/index.html"
