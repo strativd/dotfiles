9009be45dae5d7f0c103c56143e4dab37f66d807
strativd <strat.barrett@gmail.com>
2024-05-10 11:38:29 -0700

    pnpm: list, remove node_modules with rmnode

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -1,2 +1,6 @@
 # delete all node_modules folders within current project
-alias no_mo="find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +"
+alias rmnode="find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +"
+
+# pnpm list
+alias pnlg="pnpm list -g --depth=0"
+alias pnll="pnpm list --depth=0"
