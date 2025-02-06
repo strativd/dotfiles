3f404385ab9bb34472a202be300b7f4d85da6991
strativd <stratford.barrett@procurify.com>
2025-02-05 17:24:18 -0800

    🐍 activate py env if it exists

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,5 +1,5 @@
 alias pls=pls
 alias python='python3 '
 alias pyrun='python3 '
-alias pyenv='source .venv/bin/activate'
-alias pydenv='deactivate'
+alias pyact='source .venv/bin/activate'
+alias pydeact='deactivate'
--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -2,4 +2,10 @@
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+
+# Activate the virtual environment if it exists
+if [[ -d .venv ]]; then
+  source .venv/bin/activate
+fi
+
 print "🐍 → $(pyenv version)"
