5a9ba2c46a493e2159395798bc65df381908e7bc
Strat Barrett <strat.barrett@growthloop.com>
2025-10-15 16:04:34 -0700

    set cursor as editor

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -49,6 +49,7 @@ brew install git-completion
 # Install Apps
 brew install --cask raycast
 brew install --cask iterm2
+brew install --cask cursor
 brew install --cask visual-studio-code
 brew install --cask google-chrome
 brew install --cask firefox
