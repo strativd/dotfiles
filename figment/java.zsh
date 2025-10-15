4f7d3a0ccbb308c3c8a83df95026dab0bc32fa61
Strat Barrett <strat.barrett@growthloop.com>
2025-10-15 16:04:08 -0700

    🗑️ remove figment config

--- figment/java.zsh
+++
@@ -1,7 +0,0 @@
-# $ brew install openjdk
-#
-# openjdk is keg-only, which means it was not symlinked into /opt/homebrew,
-# because macOS provides similar software (which can break things...)
-#
-# Adding this to your PATH overrides the macOS dummy java program:
-# export PATH="/usr/local/opt/openjdk/bin:$PATH"
